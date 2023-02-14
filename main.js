a56dfe7be90a760cb1df47b012f6e5a1eef6cd0e
Пользователь <TokhirovIlhom>
2023-02-14 23:30:47 +0500

    Java Script 1-dars

---
+++ main.js
@@ -0,0 +1,337 @@
+var a = 9
+
+if(a > 0){
+    console.log('musbat');
+}else{
+    console.log('manfiy');
+}
+
+
+var b = 8
+
+if(b % 2 ==0){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+var d = 10
+var e = 20
+
+if(d > 0 && e > 0){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+
+var f = 30
+var g = 20
+var h = 10
+ 
+if(f > g && g > h && h < f) {
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+
+var i = 7
+
+if(100 > 7 > 0){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+var sh = 8
+
+if(b % 2 != 0){
+    console.log('juft');
+}else{
+    console.log('toq');
+}
+
+
+var j = 10
+var k = 20
+var l = 30
+var m = 40
+
+if(j > 0 && k > 0 && l > 0 && m > 0){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+
+var n = 10
+var o = 20
+var p = 30
+
+if(n > 0 || o > 0 || p > 0 ){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+var one = 'Dushanba'
+var two = 'Seshanba'
+var three = 'Chorshanba'
+var four = 'Payshanba'
+var five = 'Juma'
+var six = 'Shanba'
+var seven = 'Yakshanba'
+
+if(one > 0){
+    console.log('Dushanba');
+}
+
+if(two > 0){
+    console.log('Seshanba');
+}
+
+if(three > 0){
+    console.log('Chorshanba');
+}
+
+if(four > 0){
+    console.log('Payshanba');
+}
+
+if(five > 0){
+    console.log('Juma');
+}
+
+if(six > 0){
+    console.log('Shanba');
+}
+
+if(seven > 0){
+    console.log('Yakshanba');
+}
+
+
+var ucell = 97
+var ums = 93
+var uztelecome = 99
+var belline = 90
+
+
+
+var num = 11
+
+if(num > 0){
+    num++
+}else{
+    num--
+}
+
+
+
+// var number = 11
+
+// if(number > 0){
+//     console.log(number + 3);
+// }else{
+//     console.log(number - 2);
+// }
+
+
+var bir = 12
+var ikki = 15
+
+if(bir < ikki){
+    console.log(ikki);
+}else{
+    console.log(bir);
+}
+
+
+
+var no1 = 12
+var no2 = 19
+var no3 = 20
+
+
+if (no1 > no2 && no1 > no3){
+    console.log(no1);
+} else if(no2 > no1 && no2 > no3){
+    console.log(no2);
+} else if (no3 > no1 && no3 > no2){
+    console.log(no3);
+}
+
+
+
+
+var no4 = 12
+var no5 = 19
+var no6 = 20
+
+
+if (no4 < no5 && no4 < no6){
+    console.log(no4);
+} else if(no5 > no4 && no5 > no6){
+    console.log(no5);
+} else if (no6 > no4 && no6 > no5){
+    console.log(no6);
+}
+
+
+
+
+
+var jinsi = 'erkak'
+
+if (jinsi=='erkak'){
+    console.log('ID: 1234');
+} else {
+    console.log('Kechirasiz bu sayt erkaklar uchun');
+}
+
+
+
+
+var baho5 = 10000 * 2
+var baho4 = 8000 * 10
+var baho3 = 5000
+var baho2 = 2000
+
+if (baho5 + baho4 + baho3 + baho2 ) {
+    console.log(baho5 + baho4 + baho3 + baho2);
+} else {
+    console.log('Sen bu oy stipendiya olmaysan battar bol');
+}
+
+
+
+
+var aniqlash = 11
+
+if (aniqlash % 2 == 0 && aniqlash > 0) {
+    console.log('Musbat juft');
+} else if (aniqlash % 2 == 0 && aniqlash < 0) {
+    console.log('Manfiy juft');
+} else if (aniqlash % 2 != 0 && aniqlash > 0){
+    console.log('Musbat toq');
+} else if(aniqlash % 2 != 0 && aniqlash < 0){
+    console.log('Manfiy toq');
+}
+
+
+
+
+
+var yil = 1991
+
+if (2023 - yil){
+    console.log(2023 - yil);
+}
+
+
+
+
+var A = 5
+var B = 15
+var C = 19
+
+if (A <= B && B <= C){
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
+var son = 10
+var son1 = 11
+
+if (son % 2 == 0){
+    console.log(son1);
+} else if (son1 % 2 == 0){
+    console.log(son);
+}
+
+
+
+
+
+
+var son2 = 10
+var son3 = 14
+var son4 = 12
+
+if (son2 % 2 == 0 && son3 % 2 == 0 && son4 % 2 ==0){
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
+
+var raqam1 = 10
+var raqam2 = -11
+var raqam3 = -12
+
+
+if (raqam1 > 0) {
+    console.log('1-musbat');
+} else if (raqam2 > 0){
+    console.log('2-musbat');
+} else if (raqam3 > 0){
+    console.log('3-musbat');
+}
+
+
+
+
+
+
+
+
+
+
+
+var tekis1 = 1 
+var tekis2 = 6
+var tekis3 = 7
+
+
+if (tekis1 < tekis2 && tekis2 < tekis3){
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
+
+
+var Macbook = 2000
+var Lenovo = 2000
+var HP = 2000
+
+
+if (Macbook > 0){
+    console.log(Macbook);
+} else if (Lenovo > 0){
+    console.log(Lenovo);
+} else if (HP > 0){
+    console.log(HP);
+}
+
+
+
+
+
+
+
+for (let i = 1; i <=10; i + 0.5 ){
+    console.log(i)
+}
